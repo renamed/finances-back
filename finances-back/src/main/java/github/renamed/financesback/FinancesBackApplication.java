24a30b24d8925c667d148b9d8d26d34e54598294
Renato <renamedrj@gmail.com>
2024-03-31 14:53:02 -0300

    Initial project

---
+++ finances-back/src/main/java/github/renamed/financesback/FinancesBackApplication.java
@@ -0,0 +1,13 @@
+package github.renamed.financesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancesBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancesBackApplication.class, args);
+	}
+
+}
---
+++ finances-back/src/test/java/github/renamed/financesback/FinancesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package github.renamed.financesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
